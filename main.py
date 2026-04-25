4a4759a707745e80721fed5f26b6c9b2ff458ed7
Ashmeetha Carol <ashmeethacarol@gmail.com>
2026-04-25 18:05:52 +0530

    save image in a separate folder

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import cv2
-import numpy as np
+import datetime
+import os
+
+folder_name = "motion_images"
+if not os.path.exists(folder_name):
+    os.makedirs(folder_name)
 
 cap = cv2.VideoCapture(0)
 
@@ -7,30 +12,29 @@ ret, frame1 = cap.read()
 ret, frame2 = cap.read()
 
 while cap.isOpened():
-
-   
     diff = cv2.absdiff(frame1, frame2)
     gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray, (5, 5), 0)
     _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
     dilated = cv2.dilate(thresh, None, iterations=3)
-    contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours, _ = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-   
     for contour in contours:
         if cv2.contourArea(contour) < 2000:
             continue
-
         x, y, w, h = cv2.boundingRect(contour)
         cv2.rectangle(frame1, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        filename = os.path.join(folder_name, f"snapshot_{timestamp}.jpg")
+        cv2.imwrite(filename, frame1)
 
     cv2.imshow("Motion Detector", frame1)
 
     frame1 = frame2
     ret, frame2 = cap.read()
-
-    if cv2.waitKey(10) == 27:  # ESC
+    if not ret:
+        break
+    if cv2.waitKey(10) == 27:
         break
 
 cap.release()
